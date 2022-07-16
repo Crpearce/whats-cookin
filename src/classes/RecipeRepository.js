131dc80e79ad8f0c6f03b5a7452a63a3e7d01be0
Colby Pearce <crpearce892@gmail.com>
2022-07-16 08:58:45 -0600

    complete methods for recipe repository class and test suite

--- src/classes/RecipeRepository.js
+++ src/classes/RecipeRepository.js
@@ -2,38 +2,42 @@ import Recipe from "./Recipe"
 
 class RecipeRepository {
   constructor(recipes) {
-    
+
     // console.log(recipes[2])
      this.recipes = recipes
-     this.allRecipes = this.recipes.map(recipe => {
-     new Recipe();
-   })
-   console.log(this.allRecipes)
-    
+     // this.allRecipes = this.recipes.map(recipe => {
+     // new Recipe();
+
     //  console.log(this.ingredients)
   };
 
-
-  pickRecipeTag() {
+  pickRecipeTag(userTag) {
     // console.log(tag)
 
-
 // go into this.recipes (which is an array) and for each recipe filter through the this. recipes[i].tags and if the .tags strictly equals the tag argument return the whole recipe object in an array
+let recipesByTag = [];
 
-let findByTag = this.recipes.filter((recipe) => {
-  return recipe.tags === 'sauce';
-})
-// console.log(findByTag)
-return findByTag
-
-    // filter method
-    // return an array
-};
-  pickRecipeName(name) {
-    // filter method
-    // return an array
-  };
+const findByTag = this.recipes.filter((recipe) => {
+  recipe.tags.forEach(tag => {
+    if(tag === userTag) {
+      recipesByTag.push(recipe)
+    }
+  })
+});
+return recipesByTag
 
 };
+  pickRecipeName(userName) {
+    let recipesByName = [];
+
+    const findByName = this.recipes.filter(recipe => {
+      if(recipe.name.toLowerCase().includes(userName)){
+        recipesByName.push(recipe)
+      }
+    });
+  return recipesByName
+};
+
+}
 
 export default RecipeRepository;
