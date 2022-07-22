dfdf824dfeb3aae0fbb192f6718dc1ce3e59c1bb
Colby Pearce <crpearce892@gmail.com>
2022-07-22 08:08:46 -0600

    refactor for loops

--- src/scripts.js
+++ src/scripts.js
@@ -8,7 +8,7 @@ import Recipe from '../src/classes/Recipe';
 import RecipeRepository from '../src/classes/RecipeRepository';
 
 //global variables:
-let currentRecipe;
+let recipeMatch;
 let allRecipes;
 let recipeRepository;
 let user = null;
@@ -71,70 +71,48 @@ function createRecipe(recipe) {
 
 function viewRecipeIdeasDetails(event) {
   recipeList.innerHTML = ` `;
-  for (let i = 0; i < allRecipes.length; i++) {
-    if (allRecipes[i].id == event.target.closest(".recipe-card").id) {
-      recipeList.innerHTML += `<h1>${allRecipes[i].name}</h1>`
-    };
-  };
-
-  for (let i = 0; i < allRecipes.length; i++) {
-    if (allRecipes[i].id == event.target.closest(".recipe-card").id) {
-      let printCost = allRecipes[i].estimateIngredientCost();
-      recipeList.innerHTML += `<h3>COST $${printCost}</h3>`
-    };
-  };
-
-  for (let i = 0; i < allRecipes.length; i++) {
-    if (allRecipes[i].id == event.target.closest(".recipe-card").id) {
-      let printDirections = allRecipes[i].provideRecipeInstructions();
-      printDirections.forEach(step => {
-        recipeList.innerHTML += `<h3>${step}</h3>`
-      });
-    };
-  };
-
-  for (let i = 0; i < allRecipes.length; i++) {
-    if (allRecipes[i].id == event.target.closest(".recipe-card").id) {
-      let printIngredients = allRecipes[i].listIngredients();
-      printIngredients.forEach(ingredient => {
-        recipeList.innerHTML += `<h4>${ingredient}</h4>`
-      })
+  let recipeMatch = allRecipes.find((recipe) => {
+    if (recipe.id == event.target.closest(".recipe-card").id) {
+      return true
+    } else {
+      return false
     }
-  };
+    return recipe.id == event.target.closest(".recipe-card").id
+  })
+  recipeIdeasView.innerHTML += `<h1>${recipeMatch.name}</h1>`
+  let printCost = recipeMatch.estimateIngredientCost();
+  recipeIdeasView.innerHTML += `<h3>COST $${printCost}</h3>`
+  let printDirections = recipeMatch.provideRecipeInstructions();
+  printDirections.forEach(step => {
+    recipeIdeasView.innerHTML += `<h3>${step}</h3>`
+  })
+  let printIngredients = recipeMatch.listIngredients();
+  printIngredients.forEach(ingredient => {
+    recipeIdeasView.innerHTML += `<h4>${ingredient}</h4>`
+  })
 };
 
 function viewRecipeListDetails(event) {
   recipeList.innerHTML = ` `;
-  for (let i = 0; i < allRecipes.length; i++) {
-    if (allRecipes[i].id == event.target.closest(".view-recipe-button").id) {
-      recipeList.innerHTML += `<h1>${allRecipes[i].name}</h1>`;
-    };
-  };
-
-  for (let i = 0; i < allRecipes.length; i++) {
-    if (allRecipes[i].id == event.target.closest(".view-recipe-button").id) {
-      let printCost = allRecipes[i].estimateIngredientCost();
-      recipeList.innerHTML += `<h3>COST $${printCost}</h3>`
-    };
-  };
-
-  for (let i = 0; i < allRecipes.length; i++) {
-    if (allRecipes[i].id == event.target.closest(".view-recipe-button").id) {
-      let printDirections = allRecipes[i].provideRecipeInstructions();
-      printDirections.forEach(step => {
-        recipeList.innerHTML += `<h3>${step}</h3>`;
-      });
-    };
-  };
-
-  for (let i = 0; i < allRecipes.length; i++) {
-    if (allRecipes[i].id == event.target.closest(".view-recipe-button").id) {
-      let printIngredients = allRecipes[i].listIngredients();
-      printIngredients.forEach(ingredient => {
-        recipeList.innerHTML += `<h4>${ingredient}</h4>`;
-      });
-    };
-  };
+  let recipeMatch = allRecipes.find((recipe) => {
+    if (recipe.id == event.target.closest(".recipe-card").id) {
+      return true
+    } else {
+      return false
+    }
+    return recipe.id == event.target.closest(".recipe-card").id
+  });
+  recipeList.innerHTML += `<h1>${recipeMatch.name}</h1>`
+  let printCost = recipeMatch.estimateIngredientCost();
+  recipeList.innerHTML += `<h3>COST $${printCost}</h3>`
+  let printDirections = recipeMatch.provideRecipeInstructions();
+  printDirections.forEach(step => {
+    recipeList.innerHTML += `<h3>${step}</h3>`
+  });
+  let printIngredients = recipeMatch.listIngredients();
+  printIngredients.forEach(ingredient => {
+    recipeList.innerHTML += `<h4>${ingredient}</h4>`
+  });
 };
 
 function createRecipeIdeas(recipe) {
@@ -153,13 +131,14 @@ function createRecipeIdeas(recipe) {
 function saveRecipe(event) {
   event.target.closest(".save-button").id
   savedRecipesView.innerHTML += 'hi i am a saved recipe!'
+
   showSavedView()
   // SAVE for now... we will need this to make cards =>  user.addRecipeToList()
 };
 
 function showRecipeIdeas() {
-  recipeList.innerHTML = ' '
   // show(recipeIdeasView);
+  recipeList.innerHTML = '';
   show(recipeList);
   hide(savedRecipesView);
   allRecipes.forEach(recipe => {
@@ -168,20 +147,20 @@ function showRecipeIdeas() {
 };
 
 function showHomepage() {
-  randomCounter = 0
-  recipeList.innerHTML = ' '
-  show(recipeList)
-  hide(recipeIdeasView)
+  randomCounter = 0;
+  recipeList.innerHTML = ' ';
+  show(recipeList);
+  hide(recipeIdeasView);
   allRecipes.forEach(recipe => {
     createRecipe(recipe)
-  })
-  hide(savedRecipesView)
+  });
+  hide(savedRecipesView);
 };
 
 function showSavedView() {
-  hide(recipeList)
-  hide(recipeIdeasView)
-  show(savedRecipesView)
+  hide(recipeList);
+  hide(recipeIdeasView);
+  show(savedRecipesView);
 };
 
 function searchAllRecipes() {
